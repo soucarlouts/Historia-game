8673e82aaafe456c1973a0c213e097cdbdd66c0c
Carlota <soucarlouts@gmail.com>
2023-09-22 22:21:08 -0300

    Update musica.py

--- musica.py
+++ musica.py
@@ -2,9 +2,11 @@ import pygame
 
 pygame.init()
 pygame.mixer.music.load("Black Sabbath - Electric Funeral.mp3")
+
 pygame.mixer.music.play()
 
-while pygame.mixer.music.get_busy():
-    pass
+input("pressione enter para para a musica . . . ")
+
+pygame.mixer.music.stop()
 
 pygame.quit()
