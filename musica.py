16ba33c93695fd20504e1209a29de95640ac1769
Carlota <soucarlouts@gmail.com>
2023-09-25 12:02:43 -0300

    .

--- musica.py
+++ musica.py
@@ -1,12 +1,7 @@
 import pygame
-
 pygame.init()
 pygame.mixer.music.load("drusila_audiogame.mp3")
-
 pygame.mixer.music.play()
-
 input("pressione enter para para a musica . . . ")
-
 pygame.mixer.music.stop()
-
 pygame.quit()
---
+++ testetstest.py
@@ -0,0 +1,9 @@
+import math
+import random
+import curses
+
+class arma():
+    def sharpness(self):
+     50.50
+        
+print(arma.sharpness())
