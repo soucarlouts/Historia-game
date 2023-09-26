a4d956ec50eb832cc6d703496cb3af7bee846206
Carlota <soucarlouts@gmail.com>
2023-09-25 22:06:34 -0300

    .

--- objects.py
+++ objects.py
@@ -1,8 +1,7 @@
 class Monster:
     
-    def __init__(self, name, age, life):
+    def __init__(self, name, life):
         self.name = name
-        self.age = age
         self.life = life
 
     def get_name(self):
@@ -11,22 +10,12 @@ class Monster:
     def get_age(self):
         return self.age
     
-    def set_age(self,age):
-        self.age = age
+    def get_life(self, life):
+        return self.life 
     
-    def set_life(self, life):
-        self.life = life
     
     def damage(self):
         print("HGRUUAAAHH")
 
-Arvore = Monster("Nome: Samambaia\nIdade:", 10)
-print(Arvore.get_name(),Arvore.get_age())
-
-A2 = Monster("\nNome: Billa\nIdade:", 18)
-print(A2.get_name(), A2.get_age())
-
-A3 = Monster("Idade: \nmacaco\n", 180)
-print (A3.get_age(), A3.get_name())
-
-   
+A1 = Monster("maammaaaco", 100.0)
+print(A1.get_name(), A1.get_life(life=100.0))
