2c749b9cfe81aebdf2956715cd350a050397681e
Carlota <soucarlouts@gmail.com>
2023-09-27 12:28:50 -0300

    .

--- Codigos/keyboard.py
+++ Codigos/keyboard.py
@@ -2,3 +2,15 @@ import keyboard
 import pygame
 
 pygame.mixer.init()
+
+audio_path = ""
+pygame.mixer.music.load(audio_path)
+
+def play_audio(e):
+    if e.event_type == keyboard.KEY_DOWN:
+        pygame.mixer.music.play()
+        
+        
+keyboard.hook(play_audio)
+
+keyboard.wait("esc")        
